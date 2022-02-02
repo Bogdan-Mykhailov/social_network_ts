a4dc152499334e4c8919347f32315f9ce885cec2
Bogdan Mykhailov <bogdan_mykhailov@icloud.com>
2022-02-02 15:16:35 +0200

    add icons
    Signed-off-by: Bogdan <bogdan_mykhailov@icloud.com>

---
+++ src/components/Navigation/NawRow.tsx
@@ -0,0 +1,25 @@
+import {NavLink} from "react-router-dom";
+import classes from './navigation.module.css'
+import React from "react";
+
+
+type NawRowPropsType = {
+  url: string,
+  title: string,
+  icon?: any
+}
+
+const NavRow = (props: NawRowPropsType) => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.nawbarItem}>
+        {props.icon}
+        <h4>
+          <NavLink to={props.url}
+                   className={navData => navData.isActive ? classes.active : classes.item}>{props.title}</NavLink>
+        </h4>
+      </div>
+    </div>
+  );
+};
+export default NavRow;
--- src/components/Navigation/navigation.tsx
+++ src/components/Navigation/navigation.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 import classes from './navigation.module.css'
-import NawRow from "./NavRow/NawRow";
-import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import NawRow from "./NawRow";
+// import HomeRounded from '@mui/icons-material/HomeRounded';
+import {
+  MessageRounded,
+  MusicNoteRounded,
+  NewspaperRounded,
+  SettingsRounded,
+  SupervisedUserCircleSharp
+} from "@mui/icons-material";
 
 
 const Navigation = () => {
   return (
     <nav className={classes.nav}>
       <div >
-        <NawRow icon={<HomeRoundedIcon className={classes.icons}/>} url='/profile' title='Profile'/>
-        <NawRow url='/dialogs' title='Messages'/>
-        <NawRow url='/news' title='News'/>
-        <NawRow url='/music' title='Music'/>
-        <NawRow url='/settings' title='Settings'/>
+        <NawRow icon={<SupervisedUserCircleSharp className={classes.icons} sx={{ fontSize: 30 }}/>} url='/profile' title='Profile'/>
+        <NawRow icon={<MessageRounded className={classes.icons} sx={{ fontSize: 30 }}/>} url='/dialogs' title='Messages'/>
+        <NawRow icon={<NewspaperRounded className={classes.icons} sx={{ fontSize: 30 }}/>} url='/news' title='News'/>
+        <NawRow icon={<MusicNoteRounded className={classes.icons} sx={{ fontSize: 30 }}/>} url='/music' title='Music'/>
+        <NawRow icon={<SettingsRounded className={classes.icons} sx={{ fontSize: 30 }}/>} url='/settings' title='Settings'/>
       </div>
     </nav>
   )
