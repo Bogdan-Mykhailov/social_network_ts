f243afd465c28b2c81436ac02be305cede9fef4f
Bogdan <bogdan_mykhailov@icloud.com>
2022-02-02 12:20:25 +0200

    add icon homepage

--- src/components/Navigation/NavRow/NawRow.tsx
+++ src/components/Navigation/NavRow/NawRow.tsx
@@ -5,14 +5,17 @@ import React from "react";
 
 type NawRowPropsType = {
   url: string,
-  title: string
+  title: string,
+  icon?: any
 }
 
 const NavRow = (props: NawRowPropsType) => {
   return (
     <div>
       <div >
+        {props.icon}
         <NavLink to={props.url} className = { navData => navData.isActive ? classes.active : classes.item }>{props.title}</NavLink>
+
       </div>
     </div>
   );
--- src/components/Navigation/navigation.tsx
+++ src/components/Navigation/navigation.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 import classes from './navigation.module.css'
 import NawRow from "./NavRow/NawRow";
-
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 
 
 const Navigation = () => {
   return (
     <nav className={classes.nav}>
       <div >
-        <NawRow url='/profile' title='Profile'/>
+        <NawRow icon={<HomeRoundedIcon />} url='/profile' title='Profile'/>
         <NawRow url='/dialogs' title='Messages'/>
         <NawRow url='/news' title='News'/>
         <NawRow url='/music' title='Music'/>
